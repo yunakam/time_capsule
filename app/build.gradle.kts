f6294418bb5270a1732754850500bf25becf6e97
Yunakam <yuriko.nakamura@hotmail.com>
2025-05-09 22:13:44 +0900

    set the app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,6 @@ dependencies {
     implementation(platform(libs.compose.bom))
     implementation(libs.activity.compose)
     implementation(libs.compose.ui)
-    implementation(libs.compose.material)
     implementation(libs.compose.foundation)
     implementation(libs.compose.foundation.layout)
     implementation(libs.compose.runtime.livedata)
