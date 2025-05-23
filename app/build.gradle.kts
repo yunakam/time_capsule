bb82ccca80f19c366ac0376e38af07d0636d3a81
Yunakam <yuriko.nakamura@hotmail.com>
2025-05-23 16:24:06 +0900

    upgraded dependencies including agp and kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.parcelize")
     id("kotlin-kapt")
 }
 
 android {
     namespace = "com.example.timecapsule"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.timecapsule"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -37,12 +38,11 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        apiVersion = "2.0"
+        languageVersion = "2.0"
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
@@ -53,13 +53,14 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation(libs.material)
     implementation(libs.androidx.room.runtime)
@@ -71,7 +72,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(platform(libs.compose.bom))
     implementation(libs.activity.compose)
     implementation(libs.compose.ui)
     implementation(libs.androidx.compose.ui.text.google.fonts)
